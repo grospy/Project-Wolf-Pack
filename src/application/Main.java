d6fee6132fe81b270a774679b7b1de3a500f55ab
Shamil Karimli <523001@student.inholland.nl>
2016-02-17 21:16:23 +0100

    Main.java modified element to retrieve .fxml files added.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,12 @@
 package application;
-	
+
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 
@@ -19,11 +23,46 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 		Calculation model = new Calculation();
 		model.calculate();
 		model.print();
 	}
+	public void initRootLayout() {
+        try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("/Project-Wolf-Pack/src/view/RootLayout.fxml"));
+            BorderPane rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            Stage primaryStage = null;
+			primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+	public void showPersonOverview() {
+        try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("/Project-Wolf-Pack/src/view/GraphElementsDataset2.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            BorderPane rootLayout = null;
+			// Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+    }
 }
